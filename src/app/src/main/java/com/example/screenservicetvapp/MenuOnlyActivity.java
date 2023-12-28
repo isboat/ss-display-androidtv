8b440e483eefcb38409f275f67307859a4ab0263
isaac boateng <isboateng@hotmail.com>
2023-12-28 12:43:58 +0000

    latest

--- src/app/src/main/java/com/example/screenservicetvapp/MenuOnlyActivity.java
+++ src/app/src/main/java/com/example/screenservicetvapp/MenuOnlyActivity.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.style.StrikethroughSpan;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class MenuOnlyActivity extends AppCompatActivity {
 
@@ -16,6 +25,8 @@ public class MenuOnlyActivity extends AppCompatActivity {
     private String iconUrl;
     private ContentDataMenuItem[] menuItems;
     TableLayout tableLayout;
+
+    private static final StrikethroughSpan STRIKE_THROUGH_SPAN = new StrikethroughSpan();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,27 +57,55 @@ public class MenuOnlyActivity extends AppCompatActivity {
             TableRow tableRow = new TableRow(this);
 
             // Create TextViews for each field
+            ImageView imageView = createItemIcon(obj.getIconUrl());
             TextView textView1 = createTextView(obj.getName());
-            TextView textView2 = createTextView(obj.getPrice());
+            TextView textView2 = createPriceTextView(obj.getPrice(), obj.getDiscountPrice());
             TextView textView3 = createTextView(obj.getDescription());
-            TextView textView4 = createTextView(obj.getDiscountPrice());
 
             // Add TextViews to the TableRow
+            tableRow.addView(imageView);
             tableRow.addView(textView1);
             tableRow.addView(textView2);
             tableRow.addView(textView3);
-            tableRow.addView(textView4);
 
             // Add TableRow to the TableLayout
             tableLayout.addView(tableRow);
         }
     }
 
+    private ImageView createItemIcon(String iconUrl) {
+        ImageView imageView = new ImageView(this);
+/*
+        RelativeLayout.LayoutParams params =
+                new RelativeLayout.LayoutParams(20, 20);
+        params.width = 10;
+        params.height = 10;
+        imageView.setLayoutParams(params);*/
+        Picasso.get().load(iconUrl).into(imageView);
+        return imageView;
+    }
+
+    private TextView createPriceTextView(String price, String discountPrice) {
+        TextView textView;
+        if(ObjectExtensions.isNullOrEmpty(discountPrice)) {
+            textView = createTextView(price);
+            return textView;
+        }
+
+        String displayText = price + " " + discountPrice;
+
+        textView = createTextView(displayText);
+        textView.setText(displayText, TextView.BufferType.SPANNABLE);
+        Spannable spannable = (Spannable) textView.getText();
+        spannable.setSpan(STRIKE_THROUGH_SPAN, 0, price.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return textView;
+    }
+
     private TextView createTextView(String text) {
         TextView textView = new TextView(this);
         textView.setText(text);
         textView.setPadding(16, 16, 16, 16);
-        //textView.setGravity(Gravity.CENTER);
+        textView.setGravity(Gravity.CENTER);
         return textView;
     }
 }
